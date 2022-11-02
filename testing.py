aab344a21ff06326cc37636a7440ee6860577841
macmir <mac.mir@hotmail.com>
2022-11-02 20:42:07 +0100

    added new functions

--- testing.py
+++ testing.py
@@ -8,10 +8,18 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[O, X, O],
-            [X, O, X],
-            [O, O, X]]
+    return [[EMPTY, X, EMPTY],
+            [EMPTY, X, EMPTY],
+            [EMPTY, X, EMPTY]]
 
 board = initial_state()
-
-print(tictactoe.terminal(board))
+if tictactoe.terminal(board) == True:
+    print("Game is over!")
+    print(f"The winner is: {tictactoe.winner(board)}")
+    print(f"Utility = {tictactoe.utility(board)}")
+else:
+    print("Keep playing!")
+    act = tictactoe.actions(board)
+    print(act)
+    print(tictactoe.result(board, (0, 0)))
+    print(board)
--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 """
 Tic Tac Toe Player
 """
-
+import copy
 import math
 
 X = "X"
@@ -22,31 +22,72 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    if not board:
-        return "X"
+    if all(board):
+        return X
     else:
-        return "Y"
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_moves = []
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                action = (i, j)
+                possible_moves.append(action)
+    return possible_moves
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if action in actions(board):
+        new_board = copy.deepcopy(board)
+        move = player(board)
+        i = action[0]
+        j = action[1]
+        new_board[i][j] = move
+        return new_board
+    else:
+        raise NameError("Invalid move!")
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    if terminal(board) == True:
+        j = 0
+        while j < 3:
+            if board[j][0] == board[j][1] and board[j][0] == board[j][2] == X:
+                return X
+            if board[j][0] == board[j][1] and board[j][0] == board[j][2] == O:
+                return O
+            else:
+                j += 1
+        j = 0
+        while j < 3:
+            if board[0][j] == board[1][j] and board[0][j] == board[2][j] == X:
+                return X
+            if board[0][j] == board[1][j] and board[0][j] == board[2][j] == O:
+                return O
+            else:
+                j += 1
+
+        if board[0][0] == board[1][1] and board[0][0] == board[2][2] == X:
+            return X
+        if board[0][0] == board[1][1] and board[0][0] == board[2][2] == O:
+            return O
+        if board[2][0] == board[1][1] and board[2][0] == board[0][2] == X:
+            return X
+        if board[2][0] == board[1][1] and board[2][0] == board[0][2] == O:
+            return O
+
+    return None
 
 
 def terminal(board):
@@ -55,34 +96,48 @@ def terminal(board):
     """
     j = 0
     while j < 3:
-        if board[j][0] == board[j][1] and board[j][0] == board[j][2]:
+        if board[j][0] == board[j][1] and board[j][0] == board[j][2] != EMPTY:
             return True
         else:
             j += 1
     j = 0
     while j < 3:
-        if board[0][j] == board[1][j] and board[0][j] == board[2][j]:
+        if board[0][j] == board[1][j] and board[0][j] == board[2][j] != EMPTY:
             return True
         else:
             j += 1
 
-    if board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+    if board[0][0] == board[1][1] and board[0][0] == board[2][2] != EMPTY:
         return True
-    if board[2][0] == board[1][1] and board[2][0] == board[0][2]:
+    if board[2][0] == board[1][1] and board[2][0] == board[0][2] != EMPTY:
         return True
 
-    return False
+    empty = 0
+    for k in range(3):
+        for m in range(3):
+            if board[k][m] == EMPTY:
+                empty += 1
+
+    if empty == 0:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    return 0
